77656aed004d103fe0564d990ae3b21d6f370403
lagan <laganseba@gmail.com>
2025-01-26 15:03:45 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Intro from './Intro/Intro.jsx';
+import Card from './Card/Card.jsx'
+import Button from "./Style/Button.jsx";
+
+function App() {
+    return (
+        <>
+            <Intro/>
+            <Card/>
+            <Button/>
+        </>
+    );
+}
+
+export default App
---
+++ src/Card/Card.jsx
@@ -0,0 +1,14 @@
+import profilePic from '../assets/profilePic.jpg'
+import './card.css'
+
+function Card(){
+    return (
+        <div className="card">
+            <img className="card-image" src={profilePic} alt="profile picture"></img>
+            <h2 className="card-title">BYKUMYRDAL</h2>
+            <p className="card-text">BYKUMYRDAL is a small village in the Sognefjord, Norway. It is known for its beautiful location and stunning views of the surrounding mountains and fjords. The village is a popular destination for tourists looking to experience the natural beauty of Norway.</p>
+        </div>
+    );
+}
+
+export default Card;
